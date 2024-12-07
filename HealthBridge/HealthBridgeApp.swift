da896faf81b23933c41955cea78714fa83fdf4d5
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2024-12-07 11:09:58 -0800

    Initial Commit

---
+++ HealthBridge/HealthBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthBridgeApp.swift
+//  HealthBridge
+//
+//  Created by Myung Joon Kang on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
